c2bd6bd1ff761464f1b6296b416a6d12a2e7235a
Happy_Majima <62021893+BoxWhale@users.noreply.github.com>
2025-02-21 20:14:28 +0100

    Expand

--- test1/Print/obj/Debug/net8.0/Print.AssemblyInfo.cs
+++ test1/Print/obj/Debug/net8.0/Print.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Print")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3b6fd3dcdad9c054891eaac4b4cdf4c12c01c2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Print")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Print")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
